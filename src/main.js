8214cf91bac835ad3e2f1e884e450fdbf71cc633
web-jie <864214989@qq.com>
2020-04-12 19:49:35 +0800

    创建后台管理首页

--- src/main.js
+++ src/main.js
@@ -1,9 +1,34 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 引入element 插件
+import {
+  Button,
+  Select,
+  Container,
+  Header, 
+  Main,
+  Form,
+  Input,
+  FormItem,
+  Row,
+} from 'element-ui';
+// 导入全局样式
+import 'element-ui/lib/theme-chalk/index.css';
+
 
 Vue.config.productionTip = false
 
+//注册element ui的组件
+Vue.use(Row)
+Vue.use(FormItem)
+Vue.use(Form)
+Vue.use(Input)
+Vue.use(Button)
+Vue.use(Select)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Main)
 new Vue({
   router,
   render: h => h(App)
---
+++ src/views/Login.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="container">
+    <div class="frame">
+      <div class="logo">
+        <img src="../assets/logo.png" alt />
+      </div>
+      <el-form ref="form" class="form" :model="form" label-width="0px">
+        <el-form-item >
+          <el-input   prefix-icon="el-icon-user" v-model="form.username"></el-input>
+        </el-form-item>
+        <el-form-item >
+          <el-input prefix-icon="el-icon-lock" type="password" v-model="form.password"></el-input>
+        </el-form-item>
+        <el-row class="btns">
+          <el-button type="primary" @click="onSubmit">登录</el-button>
+          <el-button type="info">注册</el-button>
+        </el-row>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        username: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    onSubmit(){
+
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.container {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #2b3b4b;
+  .frame {
+    // border: 1px solid #000;
+    border-radius: 12px;
+    width: 450px;
+    height: 300px;
+    background: #fff;
+    position: relative;
+    .logo {
+      position: absolute;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      padding: 10px;
+      box-shadow: 0 0 10px #ddd;
+      height: 130px;
+      width: 130px;
+      border-radius: 50%;
+      border: 1px solid #eee;
+      background: #fff;
+      img {
+        border-radius: 50%;
+        width: 100%;
+        height: 100%;
+      }
+    }
+    .form{
+      width: 100%;
+      padding: 0 20px;
+      box-sizing: border-box;
+      position: absolute;
+      bottom: 0;
+      padding-bottom: 40px;
+    }
+  }
+.btns{
+  display: flex;
+  justify-content: flex-end;
+}
+}
+</style>
