4489be29e3bf87dd41faefcbfd9fbfba96a0fabf
web-jie <864214989@qq.com>
2020-04-12 23:08:08 +0800

    创建退出

--- src/main.js
+++ src/main.js
@@ -19,7 +19,9 @@ import {
   Dropdown,
   DropdownItem,
   DropdownMenu,
-
+  Menu,
+  MenuItem,
+  Submenu,
 } from 'element-ui';
 // 导入全局样式
 import 'element-ui/lib/theme-chalk/index.css';
@@ -34,6 +36,9 @@ axios.defaults.baseURL = "http://localhost:3000"
 Vue.config.productionTip = false
 
 //注册element ui的组件
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(Menu)
 Vue.use(DropdownMenu)
 Vue.use(DropdownItem)
 Vue.use(Dropdown)
