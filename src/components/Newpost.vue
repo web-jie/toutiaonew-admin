1327c1a82f8f0ce3af3b2a3ddcaae31f0a47f597
web-jie <864214989@qq.com>
2020-04-13 20:29:54 +0800

    发布文章多选按钮和图片上传组件的使用

--- src/components/Newpost.vue
+++ src/components/Newpost.vue
@@ -18,6 +18,55 @@
       <el-form-item label="内容" class="editor">
         <vue-editor v-model="form.content"></vue-editor>
       </el-form-item>
+
+      <el-form-item label="视频">
+        <!-- action：上传链接 -->
+        <!-- limit：限制上传文件的数量 -->
+        <!-- on-remove：移除文件的事件 -->
+        <el-upload
+          action="https://jsonplaceholder.typicode.com/posts/"
+          :limit="1"
+          :on-remove="handleRemove"
+        >
+          <el-button size="small" type="primary">点击上传</el-button>
+          <div slot="tip" class="el-upload__tip">只能上传mp4,avi文件，且不超过2m</div>
+        </el-upload>
+      </el-form-item>
+
+      <!-- 多选框 -->
+      <el-form-item label="栏目">
+        <el-checkbox-group v-model="form.category">
+          <el-checkbox label="A" name="type"></el-checkbox>
+          <el-checkbox label="B" name="type"></el-checkbox>
+          <el-checkbox label="C" name="type"></el-checkbox>
+          <el-checkbox label="D" name="type"></el-checkbox>
+        </el-checkbox-group>
+      </el-form-item>
+
+      <!-- 多张图片的上传 -->
+      <el-form-item label="封面">
+        <!-- action:  上传图片的地址
+				list-type: 图片上传组件使用图片列表布局
+				handlePictureCardPreview: 图片预览的事件
+        handleRemove：移除图片的事件-->
+        <el-upload
+          action="https://jsonplaceholder.typicode.com/posts/"
+          list-type="picture-card"
+          :on-preview="handlePictureCardPreview"
+          :on-remove="handleImageRemove"
+        >
+          <i class="el-icon-plus"></i>
+        </el-upload>
+        <!-- 预览图片的弹窗 -->
+        <el-dialog :visible.sync="dialogVisible">
+          <img width="100%" :src="dialogImageUrl" alt />
+        </el-dialog>
+      </el-form-item>
+
+      <!-- 提交按钮 -->
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">立即创建</el-button>
+      </el-form-item>
     </el-form>
   </div>
 </template>
@@ -32,12 +81,35 @@ export default {
       form: {
         title: "",
         type: 1,
-        content: "<h1>Some initial content</h1>"
-      }
+        content: "<h1>Some initial content</h1>",
+        category: []
+      },
+      // 图片预览要使用的属性
+      dialogImageUrl: "",
+      dialogVisible: false
     };
   },
   components: {
     VueEditor
+  },
+  methods: {
+    // 视频移除的事件
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+    },
+    // 封面图片移除的事件
+    handleImageRemove(file, fileList) {
+      console.log(file, fileList);
+    },
+    // 预览图片的事件
+    handlePictureCardPreview(file) {
+      this.dialogImageUrl = file.url;
+      this.dialogVisible = true;
+    },
+    // 发布文章的点击事件
+    onSubmit() {
+      console.log("submit");
+    }
   }
 };
 </script>
